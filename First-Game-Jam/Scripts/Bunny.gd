b16bbb73ba7eecb0b3292f25cfd5c6f39653dfed
ericpedley@gmail.com <ericpedley@gmail.com>
2020-07-24 23:29:05 -0700

    added chicken code

--- First-Game-Jam/Scripts/Bunny.gd
+++ First-Game-Jam/Scripts/Bunny.gd
@@ -14,7 +14,7 @@ func _physics_process(_delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x=Input.get_action_strength("ui_right")-Input.get_action_strength("ui_left")
 	if velocity.x<0:
-		sprite.flip_h=false;
+		sprite.flip_h=false
 	elif velocity.x>0:
 		sprite.flip_h=true
 	velocity.x=velocity.move_toward(input_vector*SPEED,100).x
